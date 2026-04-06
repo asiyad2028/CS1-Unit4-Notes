d10164d537555b56f0bf0a55153f6a3e63901bd7
asiyad2028 <asiya.dowdy2028@gbwl.org>
2026-04-06 12:52:52 +0000

    started java script notes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// SINGLE LINE COMMENT 
+/* MULTI-LINE COMMENT */
+
+/* the js code below is an example STATEMENT (instruction/command) every statement ends in a SEMICOLON*/
 console.log("hello world");
