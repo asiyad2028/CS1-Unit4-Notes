28d127908af6ab48086a998aa39b36241dd81f11
asiyad2028 <asiya.dowdy2028@gbwl.org>
2026-04-23 17:21:13 +0000

    added more to my notes

--- script.js
+++ script.js
@@ -4,6 +4,28 @@
 /* the js code below is an example STATEMENT (instruction/command) every statement ends in a SEMICOLON*/
 console.log("hello world");
 
+//VARIABLES are named storage containers for data
+
+//DECLARE a new variable (make space)
+let username; 
+
+//ASSIGN a value to an existing variable 
+username= "asiya";
+
+//DECLARE + ASSIGN new variable in one line 
+let starbucks= "caramel frappuccino";
+
+// See the value of a variable 
+console.log(username);
+console.log(starbucks);
+
+//Create a variable that holds a NUMBER 
+let luckynum=11; //numbers dont need quotes
+console.log(luckynum); 
+
+//Change the value stored in a variable 
+luckynum=3;
+console.log(luckynum); 
 
 // MATH OPERATORS (+, -, *, /)
 let age = 15;
